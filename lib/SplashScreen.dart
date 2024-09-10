982236c46300b823d0b7a0fd4da95a21bc378282
MariamWalid7 <124721041+MariamWalid7@users.noreply.github.com>
2024-09-10 13:21:52 +0300

    Update SplashScreen.dart

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -6,18 +6,15 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Expanded(
-          child: Container(
-            color: const Color(0xFF209D86),
-            child: const Center(
-              child: Text('BudgetMate',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+      body: Container(
+        color: const Color(0xFF209D86),
+        child: const Center(
+          child: Text(
+            'BudgetMate',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
